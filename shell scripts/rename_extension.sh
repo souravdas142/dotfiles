7f68bdcebf2027e7ac611851ed3a771fd666f8bf
souravdas142 <souravdas142@gmail.com>
2017-05-16 19:06:46 +0530

    batch renaming extension of files

---
+++ shell scripts/rename_extension.sh
@@ -0,0 +1,14 @@
+#renameing a file
+# taking argument as the target folder as $1
+# taking argument as the target folder as $2
+#taking argument as the extension name as $3
+
+#for i in `ls ~/Desktop/shell/test/`;
+for i in `ls $1`;
+do
+	temp=$i
+	i=`echo $i | cut -d . -f1`
+#	mv ~/Desktop/shell/test/$temp ~/Desktop/shell/test/$i.$1
+	mv $1$temp $2$i.$3
+	echo $i.$3
+done
