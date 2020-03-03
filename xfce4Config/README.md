6dec487abe210e92337e84b1ee17c0312de2a4a2
souravdas142 <souravdas142@gmail.com>
2020-03-03 12:42:41 +0530

    added details

--- xfce4Config/README.md
+++ xfce4Config/README.md
@@ -1,12 +1,19 @@
 
 ## This Script Automatically set the InterfaceName for netload plugin for Xfce4 panel
 
+### This script triggers when NetworkManager connects to a network Interface
+
 first enable service `NetworkManager-dispatcher.service`
 
 move this script to `/etc/NetworkManager/dispatcher.d/`
-change its user and group ownership to root
+change its user and group ownership to root set permission to 755
 
-For more information check : [NetworkManager 1](https://www.linuxsecrets.com/archlinux-wiki/wiki.archlinux.org/index.php/NetworkManager.html)
-[NetworkManager 2](https://wiki.archlinux.org/index.php/NetworkManager)
+`10        user="sourav" # Change User name to yours`
 
-If someone want change the conept, check : [Various Triggering 1](https://askubuntu.com/questions/436943/how-to-run-a-script-when-there-is-a-change-in-your-local-ip) & [Various Triggering 2](https://askubuntu.com/questions/258580/how-to-run-a-script-depending-on-internet-connection)
+For more information check :
+ [NetworkManager-1](https://www.linuxsecrets.com/archlinux-wiki/wiki.archlinux.org/index.php/NetworkManager.html)
+[NetworkManager-2](https://wiki.archlinux.org/index.php/NetworkManager)
+
+If someone want change the conept, check : 
+[Various Triggering-1](https://askubuntu.com/questions/436943/how-to-run-a-script-when-there-is-a-change-in-your-local-ip) 
+[Various Triggering-2](https://askubuntu.com/questions/258580/how-to-run-a-script-depending-on-internet-connection)
--- xfce4Config/setNetloadIface.sh
+++ xfce4Config/setNetloadIface.sh
@@ -1,19 +1,24 @@
+# enable service NetworkManager-dispatcther.service
+# give user and group ownership to root and set permission to 755
+# move this script to /etc/NetworkManager/dispatcher.d/
+
+# shebang to eliminate confusion if users using antoher shell
 #!/bin/bash 
 
-if [ "$2" == "up" ];then
+if [ "$2" == "up" ];then #check if interface connected with a ssid, if yes then,
 
         user="sourav" # Change User name to yours
 
-	netloadPath=/home/$user/.config/xfce4/panel/netload-10.rc
-	if [ -f $netloadPath ]; then
-		pid=$(ps aux | grep -i "netload" | grep -v "grep"|awk '{print $2}')
-		if [ "$pid" != "" ]; then
-#			cur_dev=$(ip route  | tail -1 | cut -d' ' -f3)
-			cur_dev=$1
-			pre_dev=$(awk -F'=' '/Network_Device/ {print $2}'  $netloadPath)
-			if [ "$pre_dev" != "$cur_dev" ];then
-				sed -i -e  "s/Network_Device=.*/Network_Device=$cur_dev/" $netloadPath
-				kill -1 $pid
+	netloadPath=/home/$user/.config/xfce4/panel/netload-10.rc #set where is your config file for netload
+	if [ -f $netloadPath ]; then #if netload config file exists then
+		pid=$(ps aux | grep -i "netload" | grep -v "grep"|awk '{print $2}') # search process id for netload
+		if [ "$pid" != "" ]; then #if pid exists then
+#			cur_dev=$(ip route  | tail -1 | cut -d' ' -f3) #set what is your current device using,from ip route, default gateway
+			cur_dev=$1 # by default NetworkManager-dispatcher pass interface name in $1
+			pre_dev=$(awk -F'=' '/Network_Device/ {print $2}'  $netloadPath) #what is your interface using previously?
+			if [ "$pre_dev" != "$cur_dev" ];then #if current interface change then
+				sed -i -e  "s/Network_Device=.*/Network_Device=$cur_dev/" $netloadPath # replace previous intface with current one
+				kill -1 $pid # send SIGHUP signal for reread its configuration file
 			fi
 		fi
 	fi
