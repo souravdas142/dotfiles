7f237e7fc69a7bdb25bd887b702c972877625ec3
souravdas142 <souravdas142@gmail.com>
2020-03-05 16:30:00 +0530

    user fix

--- xfce4Config/setNetloadIfaceDry.sh
+++ xfce4Config/setNetloadIfaceDry.sh
@@ -4,15 +4,24 @@
 
 # shebang to eliminate confusion if users using antoher shell
 #!/bin/bash 
+if [ ! -L /home/root ];then  # if pid own by root thn simple redirect to /root 
+	ln -fs /root/ /home/root # from /home/root, here we can treat root is a 
+else 						 # non-non root user :/
+	echo "already exists"
+fi							
+	
+
 
 # by default NetworkManager-dispatcher pass connected Iface is up or down in $2
 if [ "$2" == "up" ];then #check if IFace connected with a ssid, if yes then,
-	procString=`ps aux | grep -i ".*netload.*.so" | grep -v "grep"` # search proc string for netload
+	procString=`ps aux | grep -i ".*netload.*.so" | grep -v "grep" | head -1` # search proc string for netload
 	pid=$(echo $procString | awk '{print $2}') # extract process id for netload
 	if [ "$pid" != "" ]; then #if pid exists then
        	user=$(echo $procString | awk '{print $1}') # Extract User name 
 
-		netloadPath=/home/$user/.config/xfce4/panel/netload-10.rc # set config path for netload
+		netloadNum=$(echo $procString | awk '{print $13}') 
+		netloadPath=/home/$user/.config/xfce4/panel/netload-$netloadNum.rc # set config path for netload
+		echo "Path = $netloadPath"
 		if [ -f $netloadPath ]; then #if netload config file exists then
 	#			cur_dev=$(ip route  | tail -1 | cut -d' ' -f3) #set curr Iface using,from ip route, default gateway
 				cur_dev=$1 # by default NetworkManager-dispatcher pass interface name in $1
