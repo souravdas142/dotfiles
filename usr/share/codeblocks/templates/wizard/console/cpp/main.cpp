2d99a7f13bd5e7411bcda0c82c7167882f46e406
souravdas142 <souravdas142@gmail.com>
2020-09-01 18:25:20 +0530

    Adding codeBlocks settings and cppCodetemplates

---
+++ usr/share/codeblocks/templates/wizard/console/cpp/main.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ull unsigned long long
+#define ll long long
+#define sz(a) int((a).size())
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define rall(x) (x).rbegin, (x).rend() //reverse traversal
+#define max(a, b) (a < b ? b : a) 
+#define min(a, b) ((a > b) ? b : a) 
+#define mod 1e9 + 7 
+#define PI 3.1415926535897932384626433832795
+using namespace std;
+
+void solve(){
+
+
+
+}
+
+int main(){
+
+	fio;
+	ll tc;
+	cin>>tc;
+	
+	while(tc--){
+	
+		solve();
+
+	}
+	
+	return 0;
+
+}
+
