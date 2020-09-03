b800d28315b25f598734c42a8089ef96521f2ed2
souravdas142 <souravdas142@gmail.com>
2020-09-03 21:07:14 +0530

    modified

--- usr/share/codeblocks/templates/wizard/console/cpp/main.cpp
+++ usr/share/codeblocks/templates/wizard/console/cpp/main.cpp
@@ -1,38 +1,47 @@
 #include<bits/stdc++.h>
-#define fio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define ull unsigned long long
-#define ll long long
-#define sz(a) int((a).size())
-#define pb push_back
-#define fi first
-#define se second
-#define all(x) (x).begin(), (x).end() //Forward traversal
-#define rall(x) (x).rbegin, (x).rend() //reverse traversal
-#define max(a, b) (a < b ? b : a) 
-#define min(a, b) ((a > b) ? b : a) 
-#define mod 1e9 + 7 
-#define PI 3.1415926535897932384626433832795
 using namespace std;
+#define fio			ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ull			unsigned long long
+#define ll			long long
+#define dd			double
+#define pii			pair<int, int>
+#define pll			pair<long, long>
+#define vi			vector<int>
+#define vl			vector<long>
+#define vll			vector<ll>
+#define vvi			vector<vi >
+#define vvl			vector<vl >
+#define vvll		vector<vll >
+#define vpii		vector<pii >
+#define vpll		vector<pll >
+#define fr(i,a,b)	for(ll i=a;i<b;i++)
+#define frr(i,a,b)	for(ll i=a-1;i>=b;i--)
+#define tr(it,a)	for(auto it=a.begin();it!=a.end();it++)
+#define sz(a)		ll((a).size())
+#define pb			push_back
+#define fi			first
+#define se			second
+#define all(x)		(x).begin(), (x).end() //Forward traversal
+#define rall(x)		(x).rbegin, (x).rend() //reverse traversal
+#define max(a, b)	(a < b ? b : a) 
+#define min(a, b)	((a > b) ? b : a) 
+#define mod			1e9 + 7 
+#define PI			3.1415926535897932384626433832795
 
 void solve(){
-
-
 
 }
 
 int main(){
-
 	fio;
+	
 	ll tc;
+//	tc=1;
 	cin>>tc;
-	
 	while(tc--){
-	
 		solve();
-
 	}
-	
-	return 0;
 
+	return 0;
 }
 
