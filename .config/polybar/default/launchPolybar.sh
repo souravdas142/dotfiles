3eaa8e94fc5f05cd172d85c021c3db014b5e02b7
souravdas142 <souravdas142@gmail.com>
2021-03-18 20:55:05 +0530

    Added i3 module for all

--- .config/polybar/default/launchPolybar.sh
+++ .config/polybar/default/launchPolybar.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1-$EUID.log /tmp/polybar2-$EUID.log
-polybar example -c $HOME/.config/polybar/default/config >>/tmp/polybar1-$EUID.log 2>&1 &
+polybar main -c $HOME/.config/polybar/default/config.ini >>/tmp/polybar1-$EUID.log 2>&1 &
 
 echo "Bars launched..."
--- .config/polybar/hack/launch.sh
+++ .config/polybar/hack/launch.sh
@@ -11,5 +11,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q top -c "$DIR"/config.ini &
-polybar -q bottom -c "$DIR"/config.ini &
+polybar -q main -c "$DIR"/config.ini &
