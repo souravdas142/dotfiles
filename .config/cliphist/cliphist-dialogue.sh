0164ad15ba349994de17e3f406bff0a1b8960054
Sourav Das <souravdas142@gmail.com>
2026-01-15 12:48:42 +0530

    cliphist: add transparency and minor lunch sciript fixes
    image preview is not supported yet.
    may be on another day i made support through rofi.

--- .config/cliphist/cliphist-dialogue.sh
+++ .config/cliphist/cliphist-dialogue.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cliphist list | rofi -show-icons false -dmenu -display-columns 2 -p "Search Clipboard" -theme ./cliphist-rofi2.rasi | cliphist decode | wl-copy
+cliphist list | rofi -show-icons false -dmenu -display-columns 2 -p "Search Clipboard" -theme ~/.config/cliphist/cliphist-rofi.rasi | cliphist decode | wl-copy
+
